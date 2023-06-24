ebd6ee7163f7984f349221a0b8fa7c4756d4750a
Kaya Alp Köker <dev.kayaalpkoker@gmail.com>
2023-06-24 23:00:09 +0200

    Initial commit

---
+++ src/main/java/edu/sabanciuniv/hotelbookingapp/HotelBookingAppApplication.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.hotelbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sabanciuniv/hotelbookingapp/HotelBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.hotelbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
